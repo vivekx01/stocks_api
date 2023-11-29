a8be3e5e6832d7decfbc1b91b3fd6d79961ec070
Vivek <viveks95944@gmail.com>
2023-11-29 21:11:19 +0530

    fixed_cors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from api.endpoints import market_status_endpoint, get_stock_info_endpoint,get_stock_price_endpoint,percent_change_endpoint,nifty_fifty_endpoint,nasdaq_top_endpoint
 from celery import Celery
+from fastapi.middleware.cors import CORSMiddleware
 
 celery = Celery(
     "tasks",
@@ -11,6 +12,19 @@ celery = Celery(
 
 app = FastAPI()
 celery.config_from_object('celery_config')
+
+# Configure CORS
+origins = [
+    "http://localhost:4200",  # Add the origin of your Angular application
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # You can specify specific HTTP methods if needed
+    allow_headers=["*"],  # You can specify specific HTTP headers if needed
+)
 
 app.include_router(market_status_endpoint.router)
 app.include_router(get_stock_price_endpoint.router)
